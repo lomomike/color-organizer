6c5e8e4cbc323913fc5a2d10aa9980a9e2555a4f
Mikhail Polgun <mpolgun@ptsecurity.com>
2019-12-13 21:23:08 +0300

    Ready Orginizer

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+var webpack = require("webpack")
+var path = require("path")
+
+process.noDeprecation = true
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.join(__dirname, 'dist', 'assets'),
+        filename: "bundle.js",
+        sourceMapFilename: 'bundle.map'
+    },
+    devtool: '#source-map',
+    module: {
+        rules: [
+            {
+              test: /\.(js|jsx)$/,
+              
+              exclude: /node_modules/,
+              use: {
+                loader: 'babel-loader',
+                query: {
+                    presets: ['@babel/preset-env', '@babel/react']
+                    }
+                }
+            },
+            {
+              test: /\.css$/,
+              use: [
+                'style-loader',
+                'css-loader'
+              ]
+            },
+            {
+              test: /\.scss$/,
+              use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+              ]
+            }
+          ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            "process.env": {
+                NODE_ENV: JSON.stringify("production")
+            }
+        })
+       
+    ]
+}
