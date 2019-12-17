8fa92d3f8c58a1e9d108d4cbb57659cd2f343143
Mikhail Polgun <mpolgun@ptsecurity.com>
2019-12-17 17:18:39 +0300

    Remove TimeAge.js

--- src/components/ui/TimeAgo.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var React = require("react");
-var time_helpers_1 = require("../../lib/time-helpers");
-var TimeAgo = function (_a) {
-    var timestamp = _a.timestamp;
-    return React.createElement("div", { className: "time-ago" }, time_helpers_1.ago(timestamp));
-};
-exports["default"] = TimeAgo;
