44d23ad799211dc6887aa3120014f0349e7cb066
Mikhail Polgun <mpolgun@ptsecurity.com>
2019-12-20 17:22:39 +0300

    Made actions and constants in TS

--- src/actions.ts
+++ src/actions.ts
@@ -1,7 +1,7 @@
 import C from './constants'
 import { v4 } from 'uuid'
 
-export const addColor = (title, color) => 
+export const addColor = (title : string, color : string) => 
     ({
         type: C.ADD_COLOR,
         id: v4(),
@@ -10,13 +10,13 @@ export const addColor = (title, color) =>
         timestamp: new Date().toString()        
     })
 
-export const removeColor = id => 
+export const removeColor = (id: string) => 
     ({
         type: C.REMOVE_COLOR,
         id
     })
 
-export const rateColor = (id, rating) =>
+export const rateColor = (id: string, rating: number) =>
     ({
         type: C.RATE_COLOR,
         id,
